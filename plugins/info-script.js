246f413c7ab00ecc1caad8d4dcb30ac3d57b8aa8
Kanna Botz <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-09-18 19:06:57 +0700

    Create info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,23 +2,23 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://www.youtube.com/channel/UCOAloj1gW4OVGMtVDnNaq5w*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
+https://github.com/XyzenzKannaV1
 
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
 
-wa.me/6282127487538`
+wa.me/6283112831544`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/kannachan",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'SiestaBotzz Multi Device',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
