243087d10cf4d7315faaacd77b3d3d67e48e14de
Kanna Botz <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-09-18 19:03:23 +0700

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -6,10 +6,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaUrl: "https://www.youtube.com/channel/UCOAloj1gW4OVGMtVDnNaq5w",
     mediaType: 2,
-    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
-    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    description: "https://youtube.com/channel/UCOAloj1gW4OVGMtVDnNaq5w", 
+    title: 'siestabotzz-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig  }}})
